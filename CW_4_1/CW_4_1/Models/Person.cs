7cf3d8092e7419f9c03bea79a6692bfb58b80c15
amirwpf <hh1900dppp@gmail.com>
2023-05-18 17:13:29 +0430

    Models added!

---
+++ CW_4_1/CW_4_1/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_4_1.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NationalCode { get; set; }
+        public int Mobile { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
