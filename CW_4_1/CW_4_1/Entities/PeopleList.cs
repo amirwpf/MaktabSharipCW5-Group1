6eea42a8b04bc0628f903ad4954a5b5e6b954e2c
amirwpf <hh1900dppp@gmail.com>
2023-05-18 17:18:34 +0430

    PeopleList

---
+++ CW_4_1/CW_4_1/Entities/PeopleList.cs
@@ -0,0 +1,14 @@
+﻿using CW_4_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_4_1.Entities
+{
+    public class PeopleList
+    {
+        List<Person> people = new List<Person>();
+    }
+}
